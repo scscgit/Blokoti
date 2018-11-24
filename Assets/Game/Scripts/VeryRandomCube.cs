5d8620bbaf794137464b4b132a3e71d8edb4071c
scsc <scscgit@users.noreply.github.com>
2018-11-24 12:03:14 +0100

    Prepared basic project structure, scene hierarchy & an example cube

---
+++ Assets/Game/Scripts/VeryRandomCube.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class VeryRandomCube : MonoBehaviour
+{
+    public float speed = 2;
+
+    private Transform _transform;
+
+    // Use this for initialization
+    private void Start()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        _transform.Translate(
+            x: Input.GetAxis("Horizontal") * Time.deltaTime * speed,
+            y: 0,
+            z: Input.GetAxis("Vertical") * Time.deltaTime * speed
+        );
+    }
+}
